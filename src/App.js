80002ee8c908cf120ac2a674666fa1dd4eac3133
Zahin Afsar <43959210+Mdzahin@users.noreply.github.com>
2020-08-17 14:01:29 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style/main.scss';
-import Navbar from 'create-react-nav/nav/navSnack';
+import Navbar from 'create-react-nav';
 import Home from './files/view/home'
 import Store from './files/view/store'
 
